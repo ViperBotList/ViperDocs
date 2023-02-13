061908aaae5669bda0075d39ab70c77dc5ad0c27
Rizon <30637943+RizonFTW@users.noreply.github.com>
2023-02-13 21:48:13 +0000

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\kye02\\Desktop\\Development-shit\\ViperDocs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\kye02\\Desktop\\Development-shit\\ViperDocs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\kye02\\Desktop\\Development-shit\\ViperDocs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\kye02\\Desktop\\Development-shit\\ViperDocs\\src\\css\\custom.css'),
-];
